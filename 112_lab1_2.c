5afb99a2646baed0cf776a11a4a50ba13fe4bfbd
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 17:32:56 +0700

    A+B

--- 112_lab1_2.c
+++ 112_lab1_2.c
@@ -5,10 +5,7 @@ int main ()
     int a,b,total;
     scanf("%d" ,&a);
     scanf("%d" ,&b);
-    if(0<=a<=pow(10,9)&&0<=b<=pow(10,9))
-    {
-        total=a+b;
-        printf("%d" ,total);
-    }
+    total=a+b;
+    printf("%d" ,total);
     return 0;
 }
