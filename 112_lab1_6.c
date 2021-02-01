2a7a82385b79d397d607038af6acd8a0be448f1a
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 18:48:15 +0700

    Mutant

--- 112_lab1_6.c
+++ 112_lab1_6.c
@@ -4,10 +4,15 @@ int main()
     int i,n,m,t; //n=จำนวนการก้าวเท้าซ้ายก่อนจะไปก้าวเท้าขวา n ก้าว , m=จำนวนคร้ังการก้าวเท้าขวาก่อนที่จะกลับไปก้าวเท้าซ้าย ,  t=จำนวนก้าวที่ตอ้งใชในการเดินเพื่อถึงจุดหมาย
     char direct;
     scanf("%d %d %d" ,&n,&m,&t);
+    fflush(stdin);
     scanf("%c" ,&direct);
     for(i=1;i<=t;i++)
     {
-          if(direct == 'L')
+        if(direct == 'L')
+        {
+
+        }
+        else if(direct == 'R')
         {
 
         }
