ee4fc6f9920e2509dfc98230bd5b0cb069524d2b
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 18:14:46 +0700

    Max N

--- 112_lab1_5.c
+++ 112_lab1_5.c
@@ -3,15 +3,20 @@ int main()
 {
     int i,num,a1,max1=0,max2;
     scanf("%d" ,&num);
-    for(i=0;i<num;i++)
+    for(i=1;i<=num;i++)
     {
         scanf("%d" ,&a1);
         if(max1<a1)
         {
             max2=a1;
+            if(max2==a1)
+            {
+                max1=i;
+                break;
+            }
+
         }
     }
-    max1=i;
     printf("%d %d" ,max1,max2);
     return 0;
 }
