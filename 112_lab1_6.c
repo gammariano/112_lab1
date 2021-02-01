9108437c4c18f58edd562ff07f725f6f728a038e
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 19:02:07 +0700

    Mutant

--- 112_lab1_6.c
+++ 112_lab1_6.c
@@ -27,9 +27,22 @@ int main()
     }
     else if(feet == 'R')
     {
-
+        for(i=1;i<=t+1;i++)
+        {
+            a=a+n;
+            if(a>t)
+            {
+                direct='L';
+                break;
+            }
+            a=a+m;
+            if(a>t)
+            {
+                direct='R';
+                break;
+            }
+        }
     }
     printf("%c" ,direct);
-
     return 0;
 }
