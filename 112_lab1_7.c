c4c645ca54c78acc02ce848b392d1a6e718bc912
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 19:41:34 +0700

    a.m. & p.m.

--- 112_lab1_7.c
+++ 112_lab1_7.c
@@ -3,7 +3,15 @@ int main()
 {
     float time24,time12;
     scanf("%f" ,&time24);
-    if(time24>12.00&&time24<24.00)
+    if(time24==12.00)
+    {
+        printf("%.2f p.m." ,time24);
+    }
+    else if(time24==0.00)
+    {
+        printf("%.2f a.m." ,time24);
+    }
+    else if(time24>12.00&&time24<24.00)
     {
         time12=time24-12;
         if(time24>0.00&&time24<12.00)
@@ -25,14 +33,6 @@ int main()
         {
             printf("%.2f p.m." ,time24);
         }
-    }
-    else if(time24==12.00)
-    {
-        printf("%.2f p.m." ,time24);
-    }
-    else if(time24==0.00)
-    {
-        printf("%.2f a.m." ,time24);
     }
     return 0;
 }
