f1df2566cd61ac4b5b10a056d1b44233bb58cf98
Kornrawoot Waramanuchai <kornrawoot.w@ku.th>
2021-02-01 17:31:11 +0700

    Create 112_lab1_2.c

---
+++ 112_lab1_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main ()
+{
+    int a,b,total;
+    scanf("%d" ,&a);
+    scanf("%d" ,&b);
+    if(0<=a<=pow(10,9)&&0<=b<=pow(10,9))
+    {
+        total=a+b;
+        printf("%d" ,total);
+    }
+    return 0;
+}
